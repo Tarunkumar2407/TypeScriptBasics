3189a0976fae9537e54f3b5b2ed89d0e0dbf4ead
Tarun Kumar <tarunkumar2407@gmail.com>
2023-08-29 12:11:05 +0530

    function

---
+++ app.js
@@ -0,0 +1,7 @@
+// let num1:number = 10;
+// let num2:number = 20;
+// console.log(num1 + num2)
+function sum(num1, num2) {
+    return num1 + num2;
+}
+console.log(sum("20", 30));
---
+++ app.ts
@@ -0,0 +1,10 @@
+// let num1:number = 10;
+// let num2:number = 20;
+
+// console.log(num1 + num2)
+
+function sum(num1:number, num2:number){
+    return num1 + num2
+}
+
+console.log(sum("20", 30))
