27930d79bad813589bc7362859076b2b798f9b80
Tarun Kumar <tarunkumar2407@gmail.com>
2023-08-30 13:07:40 +0530

    function

--- function.js
+++ function.js
@@ -1,10 +1,24 @@
-"use strict";
 // the `: number` here specifies that this function returns a number
 // function getTime(): number {
 //     return new Date().getTime();
 //   }
 // console.log(getTime())
-const func = () => {
-    console.log("hello world");
-};
-func();
+// const func = (): void => {
+//   console.log("hello world")
+// }
+// func()
+// function multiply(a:number, b:number):number {
+//   return a * b;
+// }
+// console.log(multiply(4, 5))
+//optional parameter
+function add(a, b, c) {
+    if (c) {
+        return a + b + c;
+    }
+    else {
+        return a + b;
+    }
+}
+console.log(add(20, 30));
+console.log(add(20, 30, 40));
--- function.ts
+++ function.ts
@@ -4,7 +4,24 @@
 //   }
 // console.log(getTime())
 
-const func = (): void => {
-  console.log("hello world")
+// const func = (): void => {
+//   console.log("hello world")
+// }
+// func()
+
+// function multiply(a:number, b:number):number {
+//   return a * b;
+// }
+// console.log(multiply(4, 5))
+
+//optional parameter
+function add(a:number, b:number, c?:number){
+  if(c){
+    return a + b + c;
+  }
+  else{
+    return a + b;
+  }
 }
-func()
+console.log(add(20,30))
+console.log(add(20,30,40))
