5a9f41491cc08476c95fe55463ba743e1f0b0763
Tarun Kumar <tarunkumar2407@gmail.com>
2023-08-30 14:35:35 +0530

    tuple

--- tuple.js
+++ tuple.js
@@ -1,3 +1,30 @@
 // let ourTuple: [number, boolean, string] = [10, true, "name"]
-var customer = ["Tarun", 24, true];
-console.log(customer);
+// console.log(ourTuple)
+// console.log(ourTuple[0])
+// console.log(ourTuple[1])
+// console.log(ourTuple[2])
+// type TupleArray = [string, number, boolean]
+// const customer: TupleArray = ["Tarun", 24, true]
+// console.log(customer)
+// type TupleArray =[string, number, boolean]
+// const customer: TupleArray = ["Tarun", 24, true]
+// console.log(customer)
+// let ourTuple: [number, boolean, string] = [10, true, "name"]
+// ourTuple.push(false, "sounds good")
+// console.log(ourTuple)
+// let ourTuple: readonly [number, boolean, string] = [10, true, "name"]
+// ourTuple.push(false, "sounds good", "readonly")
+// console.log(ourTuple)
+// define our tuple
+var ourTuple;
+// initialize correctly
+// ourTuple = [5, false, 'Coding God was here'];
+// // We have no type safety in our tuple for indexes 3+
+// ourTuple.push('Something new and wrong');
+// // instead use our readonly tuple
+// const ourReadonlyTuple: readonly [number, boolean, string] = [5, true, 'The Real Coding God'];
+// // throws error as it is readonly.
+// ourReadonlyTuple.push('Coding God took a day off');
+// console.log(ourReadonlyTuple)
+var graph = [55.2, 41.3];
+console.log(graph);
--- tuple.ts
+++ tuple.ts
@@ -5,7 +5,43 @@
 // console.log(ourTuple[1])
 // console.log(ourTuple[2])
 
-type TupleArray = [string, number, boolean]
+// type TupleArray = [string, number, boolean]
 
-const customer: TupleArray = ["Tarun", 24, true]
-console.log(customer)
+// const customer: TupleArray = ["Tarun", 24, true]
+// console.log(customer)
+
+// type TupleArray =[string, number, boolean]
+
+// const customer: TupleArray = ["Tarun", 24, true]
+// console.log(customer)
+
+
+// let ourTuple: [number, boolean, string] = [10, true, "name"]
+// ourTuple.push(false, "sounds good")
+
+// console.log(ourTuple)
+
+
+// let ourTuple: readonly [number, boolean, string] = [10, true, "name"]
+// ourTuple.push(false, "sounds good", "readonly")
+
+// console.log(ourTuple)
+
+// define our tuple
+let ourTuple: [number, boolean, string];
+
+// initialize correctly
+// ourTuple = [5, false, 'Coding God was here'];
+
+// // We have no type safety in our tuple for indexes 3+
+// ourTuple.push('Something new and wrong');
+
+// // instead use our readonly tuple
+// const ourReadonlyTuple: readonly [number, boolean, string] = [5, true, 'The Real Coding God'];
+
+// // throws error as it is readonly.
+// ourReadonlyTuple.push('Coding God took a day off');
+// console.log(ourReadonlyTuple)
+
+const graph: [x: number, y: number] = [55.2, 41.3];
+console.log(graph)
